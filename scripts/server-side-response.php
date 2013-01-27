fa0b7e384880932144fcf63fa063e117270f46f8
PJ Dietz <pjdietz@gmail.com>
2013-01-27 17:14:46 -0500

    Add scripts directory with simple single-file examples

---
+++ scripts/server-side-response.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Server-side Response
+ *
+ * This script creates and outputs a response from the server.
+ */
+
+// Include the autoload script.
+require_once('../vendor/autoload.php');
+
+use \pjdietz\WellRESTed\Response;
+
+// Create a new Response instance.
+$resp = new Response();
+$resp->statusCode = 200;
+$resp->setHeader('Content-Type', 'text/plain');
+$resp->body = 'This is a response.';
+$resp->respond();
+exit;
